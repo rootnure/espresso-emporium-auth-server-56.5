e1288e8473dc444a6c062a697514079a54a66843
rootnure <nur.diu.2791@gmail.com>
2023-10-19 00:00:58 +0600

    DELETE api for single user data

--- index.js
+++ index.js
@@ -101,6 +101,13 @@ async function run() {
             res.send(users);
         })
 
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
